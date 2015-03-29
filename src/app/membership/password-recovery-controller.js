0f7fe2719874af94f8e21f12af9c3cd1743da22c
Darjan Bogdan <darjan@mono.hr>
2015-03-29 21:40:48 +0200

    Password recovery

--- src/app/membership/password-recovery-controller.js
+++ src/app/membership/password-recovery-controller.js
@@ -1,3 +1,36 @@
 /**
  * Created by Darjan on 28.3.2015..
  */
+angular.module('membershipblog')
+  .controller('PasswordRecoveryController', function($scope, $state, baasicPasswordRecoveryService, baasicRecaptchaService){
+    'use strict';
+
+    var vm = {};
+    $scope.vm = vm;
+
+    vm.message = '';
+
+    vm.recoveryData = {};
+    vm.recoveryData.challengeIdentifier = '';
+    vm.recoveryData.challengeResponse = '';
+    vm.recoveryData.recoverUrl = $state.href('password-change', {}, { absolute: true }) + '?passwordRecoveryToken={passwordRecoveryToken}';
+
+    vm.recoverPassword = function(){
+      vm.recoveryData.challengeIdentifier = baasicRecaptchaService.challenge();
+      vm.recoveryData.challengeResponse = baasicRecaptchaService.response();
+
+      if (vm.recoveryData.challengeResponse === '') {
+        vm.message = 'Captcha code is required.';
+        return;
+      }
+
+      baasicPasswordRecoveryService.requestReset(vm.recoveryData)
+        .success(function(){
+          vm.message = 'An email with a password change link has been successfully sent.';
+        })
+        .error(function(data, status){
+          vm.message = data + ': ' + status;
+        });
+
+    };
+  });
